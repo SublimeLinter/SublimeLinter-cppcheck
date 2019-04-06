d624a1e638cf6f1fca7b7d8966f3ab7c6c9f7300
Ben Combee <combee@techwood.org>
2019-04-06 17:21:19 -0500

    Use explicit template instead of 'gcc'
    cppcheck 1.84 changed the meaning of --template=gcc to add a second line of output that confuses the linter plugin, so explicitly request the old format
    
    see https://github.com/danmar/cppcheck/commit/f058d9ad083a6111e9339b4b3506c5da7db579e0 for the details of that change

--- linter.py
+++ linter.py
@@ -2,7 +2,7 @@ from SublimeLinter.lint import Linter, util
 
 
 class Cppcheck(Linter):
-    cmd = ('cppcheck', '--template=gcc', '--inline-suppr', '--quiet', '${args}', '${file}')
+    cmd = ('cppcheck', '--template={file}:{line}: {severity}: {message}', '--inline-suppr', '--quiet', '${args}', '${file}')
     regex = (
         r'^(?P<file>(:\\|[^:])+):(?P<line>\d+):((?P<col>\d+):)?\s+'
         r'((?P<error>error)|(?P<warning>warning|style|performance|portability|information)):\s+'
