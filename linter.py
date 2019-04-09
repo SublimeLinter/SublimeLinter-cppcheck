a3ee55cf4d9182247dcc7a42b0336c467dce9e3e
Ben Combee <combee@techwood.org>
2019-04-08 20:01:23 -0500

    Add cppcheck issue id as code field

--- linter.py
+++ linter.py
@@ -4,16 +4,16 @@ from SublimeLinter.lint import Linter, util
 class Cppcheck(Linter):
     cmd = (
         'cppcheck',
-        '--template={file}:{line}: {severity}: {message}',
+        '--template={file}:{line}:{column}:{severity}:{id}:{message}',
         '--inline-suppr',
         '--quiet',
         '${args}',
         '${file}'
     )
     regex = (
-        r'^(?P<file>(:\\|[^:])+):(?P<line>\d+):((?P<col>\d+):)?\s+'
-        r'((?P<error>error)|(?P<warning>warning|style|performance|portability|information)):\s+'
-        r'(?P<message>.+)'
+        r'^(?P<file>(:\\|[^:])+):(?P<line>\d+):((?P<col>\d+):)'
+        r'((?P<error>error)|(?P<warning>warning|style|performance|portability|information)):'
+        r'(?P<code>\w+):(?P<message>.+)'
     )
     error_stream = util.STREAM_BOTH  # linting errors are on stderr, exceptions like "file not found" on stdout
     on_stderr = None  # handle stderr via split_match
