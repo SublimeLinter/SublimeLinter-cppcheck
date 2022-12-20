0f83a57113d3316bb9262851c6a66af2737d6295
Jack Cherng <jfcherng@gmail.com>
2022-12-21 02:34:23 +0800

    refactor: differentiate C and C++ setup (#24)
    fixes undefined

--- linter.py
+++ linter.py
@@ -1,25 +1,47 @@
 from SublimeLinter.lint import Linter, util
 
+CMD = (
+    "cppcheck",
+    "--template={file}:{line}:{column}:{severity}:{id}:{message}",
+    "--inline-suppr",
+    "--quiet",
+    "${args}",
+    "${file}",
+)
+
+REGEX = (
+    r"^(?P<filename>(:\\|[^:])+):(?P<line>\d+):((?P<col>\d+):)"
+    r"((?P<error>error)|(?P<warning>warning|style|performance|portability|information)):"
+    r"(?P<code>\w+):(?P<message>.+)"
+)
+
 
 class Cppcheck(Linter):
-    cmd = (
-        'cppcheck',
-        '--template={file}:{line}:{column}:{severity}:{id}:{message}',
-        '--inline-suppr',
-        '--quiet',
-        '${args}',
-        '${file}'
-    )
-    regex = (
-        r'^(?P<filename>(:\\|[^:])+):(?P<line>\d+):((?P<col>\d+):)'
-        r'((?P<error>error)|(?P<warning>warning|style|performance|portability|information)):'
-        r'(?P<code>\w+):(?P<message>.+)'
-    )
+    cmd = CMD
+    regex = REGEX
     error_stream = util.STREAM_BOTH  # linting errors are on stderr, exceptions like "file not found" on stdout
     on_stderr = None  # handle stderr via split_match
-    tempfile_suffix = '-'
+    tempfile_suffix = "-"
+
     defaults = {
-        'selector': 'source.c, source.c++',
-        '--std=,+': [],  # example ['c99', 'c89']
-        '--enable=,': 'style',
+        "selector": "source.c",
+        "--language=": "c",
+        "--std=,+": [],  # example ['c89', 'c99', 'c11']
+        "--enable=,": "style",
+    }
+
+
+class CppcheckPlus(Linter):
+    cmd = CMD
+    regex = REGEX
+    error_stream = util.STREAM_BOTH  # linting errors are on stderr, exceptions like "file not found" on stdout
+    on_stderr = None  # handle stderr via split_match
+    tempfile_suffix = "-"
+
+    name = 'cppcheck++'
+    defaults = {
+        "selector": "source.c++",
+        "--language=": "c++",
+        "--std=,+": [],  # example ['c++03', 'c++11', 'c++14', 'c++17', 'c++20']
+        "--enable=,": "style",
     }
